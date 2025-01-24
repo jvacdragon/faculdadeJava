b2940e93083f8ed0bc364e56761965eb63d6005f
João <95105377+jvacdragon@users.noreply.github.com>
2025-01-24 11:11:04 -0300

    Atualizar o Main.java

--- P2nX/src/Main.java
+++ P2nX/src/Main.java
@@ -4,25 +4,25 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         ArrayList<PessoaIMC> listaPessoas;
-        Homem h1 = new Homem("joao", "vitor", 25,5,2003,18233437751l, 75f,
+        Homem h1 = new Homem("joao", "vitor", 25,5,2003,74842599006l, 75f,
                 1.73f);
-        Homem h2 = new Homem("joao", "Aguiar", 19,7,2000,18233437751l, 70f,
+        Homem h2 = new Homem("joao", "Aguiar", 19,7,2000,74842599006l, 70f,
                 1.76f);
-        Homem h3 = new Homem("joao", "costa", 4,7,1996,18233437751l, 77f,
+        Homem h3 = new Homem("joao", "costa", 4,7,1996,74842599006l, 77f,
                 1.68f);
-        Homem h4 = new Homem("joao", "pedro", 7,2,2006,18233437751l, 69f,
+        Homem h4 = new Homem("joao", "pedro", 7,2,2006,74842599006l, 69f,
                 1.69f);
-        Homem h5 = new Homem("joao", "guilherme", 3,2,2010,18233437751l, 58f,
+        Homem h5 = new Homem("joao", "guilherme", 3,2,2010,74842599006l, 58f,
                 1.65f);
-        Mulher m1 = new Mulher("maria", "eduarda", 2,5,2003,18233437751l, 65f,
+        Mulher m1 = new Mulher("maria", "eduarda", 2,5,2003,74842599006l, 65f,
                 1.73f);
-        Mulher m2 = new Mulher("maria", "vitoria", 3,5,2006,18233437751l, 63f,
+        Mulher m2 = new Mulher("maria", "vitoria", 3,5,2006,74842599006l, 63f,
                 1.65f);
-        Mulher m3 = new Mulher("maria", "luisa", 5,5,2003,18233437751l, 58f,
+        Mulher m3 = new Mulher("maria", "luisa", 5,5,2003,74842599006l, 58f,
                 1.59f);
-        Mulher m4 = new Mulher("maria", "ana", 7,5,1999,18233437751l, 55f,
+        Mulher m4 = new Mulher("maria", "ana", 7,5,1999,74842599006l, 55f,
                 1.61f);
-        Mulher m5 = new Mulher("maria", "sem criatividade", 25,5,2000,18233437751l, 57f,
+        Mulher m5 = new Mulher("maria", "sem criatividade", 25,5,2000,74842599006l, 57f,
                 1.60f);
         listaPessoas = new ArrayList<>(Arrays.asList(h1,h2,h3,h4,h5,m1,m2,m3,m4,m5));
         MinhaListaOrdenavel minhaListaOrdenavel = new MinhaListaOrdenavel();
