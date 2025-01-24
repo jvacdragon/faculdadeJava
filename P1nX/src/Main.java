9d65795c9958e3c3024f7ef9a65ac8f8be9bfa3e
João <95105377+jvacdragon@users.noreply.github.com>
2025-01-24 11:07:47 -0300

    Atualizar o Main.java

--- P1nX/src/Main.java
+++ P1nX/src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("abcdef".substring(0, 2) + "abcdef".substring(3));
-        System.out.println(new ValidaCPF("").cpfFormat("182.334.377-51"));
+        System.out.println(new ValidaCPF("").cpfFormat("748.425.990-06"));
         System.out.println(Pessoa.numPessoas());
-        Pessoa a = new Pessoa("joao", "vitor", 25,5,2003,18233437751l, 75f,
+        Pessoa a = new Pessoa("joao", "vitor", 25,5,2003,74842599006l, 75f,
                 1.73f);
         System.out.println(a.toString());
         Pessoa b = new Pessoa("joao", "vitor", 13,5,2000,18233437751l, 75f,
                 1.73f);
         System.out.println(Pessoa.numPessoas());
         System.out.println(b.getIdade());
-        Homem c = new Homem("joao", "vitor", 26,7,1960,18233437751l, 75f,
+        Homem c = new Homem("joao", "vitor", 26,7,1960,74842599006l, 75f,
                 1.73f);
         System.out.println(c.toString());
     }
